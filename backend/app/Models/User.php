011727b70f42221cbdc541a98e28c6a67c7668fb
Mackky <stplus.checkin@gmail.com>
2026-01-15 15:05:58 +0700

    Update api.php and frontend pages

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,6 +12,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// ต้องมีบรรทัดนี้เพื่อให้ Next.js เรียกใช้งานได้
-Route::get('/test-connection', function () {
-    return response()->json([
-        'message' => 'สำเร็จ! ระบบออฟฟิสเชื่อมต่อกับ Laravel ได้แล้ว 🎉',
-        'status' => 'success',
-        'time' => now()->toDateTimeString()
-    ]);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });
